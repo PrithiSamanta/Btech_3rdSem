eccd75a54e922bb46df27706a93246c69916a361
PrithiSamanta <samantaprithi@gmail.com>
2026-08-05 01:03:53 +0530

    Lab done

---
+++ FrontEndTech/lab14/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import HelloFunc from './HelloFunc'
+import HelloClass from './HelloClass'
+
+function App() {
+
+  return (
+    <>
+    <div>Hello world from App</div>
+    <HelloFunc/>
+    <HelloClass/>
+    </>
+  )
+}
+
+export default App
---
+++ FrontEndTech/lab14/src/HelloFunc.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HelloFunc() {
+  return (
+    <div>Hello world from function</div>
+  )
+}
+
+export default HelloFunc
